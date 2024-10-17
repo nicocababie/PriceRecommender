f8987fbf0d6767b110a0016a89bf760e35f5dc03
andynaistat <naistat.a@gmail.com>
2024-10-17 00:15:48 -0300

    add purchase controller

---
+++ backend/src/controller/purchaseController.ts
@@ -0,0 +1,53 @@
+import { inject, injectable } from "inversify";
+import { IpurchaseService } from "../serviceInterface/IpurchaseService";
+import { Request, Response } from 'express';
+
+@injectable()
+export class purchaseController {
+    _purchaseService: IpurchaseService;
+    constructor(@inject("IpurchaseService") purchaseService: IpurchaseService) {
+        this._purchaseService = purchaseService;
+    }
+
+    createPurchase = async (req: Request, res: Response) => {
+        try {
+            let data = req.body;
+            let result = await this._purchaseService.createPurchase(data);
+            
+            res.status(201).json({
+                message: "Purchase created successfully",
+                data: result
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Error creating purchase",
+                error: error.message
+            });
+        }
+    }
+
+    getPurchase = async (req: Request, res: Response) => {
+        try {
+            let purchaseId = req.params.id;
+            let result = await this._purchaseService.getPurchase(purchaseId);
+            
+            if (!result) {
+                return res.status(404).json({
+                    message: "Purchase not found"
+                });
+            }
+
+            res.status(200).json({
+                message: "Purchase retrieved successfully",
+                data: result
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Error retrieving purchase",
+                error: error.message
+            });
+        }
+    }
+}
+
+export default purchaseController;

4af6c6b802003d13e46a739d8a6294e58d571c54
Nicolas Cababie <cababienico@gmail.com>
2024-10-25 23:43:54 -0300

    android - refactor address map

--- mobile/PriceRecommender/app/src/main/java/com/example/pricerecommender/ui/screen/address/AddressManualEntryScreen.kt
+++ mobile/PriceRecommender/app/src/main/java/com/example/pricerecommender/ui/screen/address/AddressManualEntryScreen.kt
@@ -1,6 +1,5 @@
 package com.example.pricerecommender.ui.screen.address
 
-import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,12 +8,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
@@ -31,12 +32,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pricerecommender.R
 import com.example.pricerecommender.services.geocodeLocation
 import com.example.pricerecommender.ui.screen.components.CustomOutlinedButton
 import com.example.pricerecommender.ui.theme.PriceRecommenderTheme
@@ -66,85 +69,91 @@ fun AddressManualEntryScreen(
     val context = LocalContext.current
     val keyboardController = LocalSoftwareKeyboardController.current
     var expanded by remember { mutableStateOf(false) }
+    var isMapLoaded by remember { mutableStateOf(false) }
 
-    Column (
+    Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = Modifier.fillMaxSize()
     ){
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+        Text(
+            text = "Enter an address",
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(top = 24.dp)
+        )
+        OutlinedTextField(
+            value = currentAddress,
+            onValueChange = { updateCurrentAddress(it) },
+            singleLine = true,
+            label = { Text(stringResource(R.string.address)) },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done,
+                capitalization = KeyboardCapitalization.Sentences
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    keyboardController?.hide()
+                }
+            ),
+        )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
         ){
             Text(
-                text = "Enter an address",
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp
+                text = currentDepartment,
             )
-            OutlinedTextField(
-                value = currentAddress,
-                onValueChange = { updateCurrentAddress(it) },
-                singleLine = true,
-                label = { Text("Address") },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Done,
-                    capitalization = KeyboardCapitalization.Sentences
-                ),
-                keyboardActions = KeyboardActions(
-                    onDone = {
-                        keyboardController?.hide()
-                    }
-                ),
-            )
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
-            ){
-                Text(
-                    text = currentDepartment,
-                )
-                OutlinedButton(
-                    onClick = { expanded = !expanded },
-                    border = BorderStroke(0.dp, Color.Transparent)
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.KeyboardArrowDown,
-                        contentDescription = "Select department")
-                }
-            }
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxWidth()
+            OutlinedButton(
+                onClick = { expanded = !expanded },
+                border = BorderStroke(0.dp, Color.Transparent)
             ) {
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(160.dp)
-                ) {
-                    departments.forEach {
-                        OutlinedButton(
-                            onClick = {
-                                updateCurrentDepartment(it)
-                                expanded = false
-                            },
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            border = BorderStroke(0.dp, Color.Transparent)
-                        ) {
-                            Text(text = it)
-                        }
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = stringResource(R.string.select_department)
+                )
+            }
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(160.dp)
+            ) {
+                departments.forEach {
+                    OutlinedButton(
+                        onClick = {
+                            updateCurrentDepartment(it)
+                            expanded = false
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        border = BorderStroke(0.dp, Color.Transparent)
+                    ) {
+                        Text(
+                            text = it,
+                            modifier = Modifier.fillMaxWidth()
+                        )
                     }
                 }
             }
+        }
 
-            if (!expanded){
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(12.dp)
-                ) {
-                    Button(onClick = {
+        if (!expanded){
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                modifier = Modifier.padding(horizontal = 12.dp)
+            ) {
+                CustomOutlinedButton(
+                    text = stringResource(R.string.check),
+                    enabled = currentAddress != "" && currentDepartment != stringResource(R.string.select_department),
+                    onClick = {
                         keyboardController?.hide()
                         val input = "${currentAddress}, ${currentDepartment}"
                         geocodeLocation(context, input) { latLng ->
@@ -154,49 +163,56 @@ fun AddressManualEntryScreen(
                                 updateCameraPosition(coord, 15f)
                                 cameraPosition.position =
                                     CameraPosition.fromLatLngZoom(currentCoord, 15f)
-                            } ?: Toast.makeText(
-                                context,
-                                "Invalid address format",
-                                Toast.LENGTH_SHORT
-                            ).show()
+                            }
                         }
-                    }) {
-                        Text(text = "Check")
-                    }
-                    Button(onClick =
-                    {
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+                CustomOutlinedButton(
+                    text = stringResource(R.string.cancel),
+                    onClick = {
                         onCancelClick()
                         emptyState()
-                    }
-                    ) {
-                        Text(text = "Cancel")
-                    }
-                }
+                    },
+                    modifier = Modifier.weight(1f)
+                )
             }
-            if (currentCoord != LatLng(0.0, 0.0)) {
-                val markerState = MarkerState(position = currentCoord)
+        }
+        if (currentCoord != LatLng(0.0, 0.0)) {
+            val markerState = MarkerState(position = currentCoord)
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ){
                 GoogleMap(
                     cameraPositionState = cameraPosition,
+                    onMapLoaded = { isMapLoaded = true },
                     modifier = Modifier
                         .fillMaxSize(0.9f)
                         .clip(RoundedCornerShape(12.dp))
                 ) {
                     Marker(state = markerState)
                 }
-                CustomOutlinedButton(
-                    text = "Submit",
-                    onClick = {
-                        onAddAddressClick(
-                            currentAddress,
-                            currentCoord.latitude,
-                            currentCoord.longitude
-                        )
-                        emptyState()
-                    }
-                )
+                if (!isMapLoaded) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .wrapContentSize()
+                    )
+                }
             }
-
+            CustomOutlinedButton(
+                text = stringResource(R.string.submit),
+                onClick = {
+                    onAddAddressClick(
+                        currentAddress,
+                        currentCoord.latitude,
+                        currentCoord.longitude
+                    )
+                    emptyState()
+                }
+            )
         }
+
     }
 }
 
